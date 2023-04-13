3c0fe17e16f629e03a466ae9a04ff33a8fa780ac
vic-147 <dmkorps@gmail.com>
2023-04-13 12:54:37 -0400

    :rocket: Listo para deploy

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 
 const appSettings = {
@@ -24,14 +26,49 @@ addButtonEl.addEventListener("click", function () {
   console.log(`${inputValue} added to database`);
 
   clearInputFieldEl();
+});
 
-  appendItemToShoppingListEl(inputValue);
+//traer datos del DB
+onValue(listaDeComprasDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearShoppingListEl();
+
+    for (let index = 0; index < itemsArray.length; index++) {
+      let currentItem = itemsArray[index];
+      // let currebtItemId = currentItem[0];
+      // let currebtItemValue = currentItem[1];
+
+      appendItemToShoppingListEl(currentItem);
+    }
+    // console.log(itemsArray);
+  } else {
+    shoppingListEl.innerHTML = "No item here... yet";
+  }
 });
 
 function clearInputFieldEl() {
   inputFieldEl.value = "";
 }
 
-function appendItemToShoppingListEl(itemValue) {
-  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
+function appendItemToShoppingListEl(item) {
+  // shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newLiEl = document.createElement("li");
+  newLiEl.textContent = itemValue;
+  shoppingListEl.append(newLiEl);
+
+  newLiEl.addEventListener("click", function () {
+    console.log(`${itemID} deleted`);
+
+    let exactLocationOfItemInDB = ref(database, `listaDeCompras/${itemID}`);
+    remove(exactLocationOfItemInDB);
+  });
 }
